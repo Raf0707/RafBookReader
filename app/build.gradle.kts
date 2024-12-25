97d1d69a11a29de755ca5c5253df467d837f4638
ByteFlipper <byteflipper.business@gmail.com>
2024-12-25 16:34:56 +0100

    Final changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.byteflipper.everbook"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.4.0"
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/com/byteflipper/everbook/presentation/screens/start/components/StartSourceCodeScreen.kt
+++ app/src/main/java/com/byteflipper/everbook/presentation/screens/start/components/StartSourceCodeScreen.kt
@@ -1,17 +1,14 @@
 package com.byteflipper.everbook.presentation.screens.start.components
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -21,7 +18,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.byteflipper.everbook.R
 import com.byteflipper.everbook.presentation.screens.about.data.AboutViewModel
-import com.byteflipper.everbook.presentation.screens.settings.nested.appearance.components.subcategories.ThemePreferencesSubcategory
 
 fun LazyListScope.startSourceCodeScreen() {
     item {
