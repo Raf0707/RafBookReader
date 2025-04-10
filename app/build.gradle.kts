af2a13b8171c6f44fee9289f022b069771f2be99
Raf0707 <Raf_0707@mail.ru>
2025-04-10 05:07:54 +0300

    v2.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "raf.console.chitalka"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "2.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
